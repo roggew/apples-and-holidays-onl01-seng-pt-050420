30aba0226d8672e84f09b1288299296adb4293a4
William Rogge <williamroggeiv@gmail.com>
2020-05-16 23:04:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,6 +52,7 @@ return array.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
+  
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
